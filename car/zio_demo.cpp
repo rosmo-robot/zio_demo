0f78d2317a6ed3516c77fc4f5c84e9b42f13833b
samuk <sam@bristolwireless.net>
2023-12-04 23:22:16 +0000

    Update zio_demo.cpp

--- car/zio_demo.cpp
+++ car/zio_demo.cpp
@@ -1,202 +1,138 @@
+/*************************************************** 
+  This is an example for our Adafruit 16-channel PWM & Servo driver
+  Servo test - this will drive 16 servos, one after the other
+
+  Pick one up today in the adafruit shop!
+  ------> http://www.adafruit.com/products/815
+
+  These displays use I2C to communicate, 2 pins are required to  
+  interface. For Arduino UNOs, thats SCL -> Analog 5, SDA -> Analog 4
+
+  Adafruit invests time and resources providing this open source code, 
+  please support Adafruit and open-source hardware by purchasing 
+  products from Adafruit!
+
+  Written by Limor Fried/Ladyada for Adafruit Industries.  
+  BSD license, all text above must be included in any redistribution
+  
+  PCA9685  --  TB6612       PCA9685  --   TB6612
+  0        --  PWMA1         8       --   A2IN1
+  1        --  A1IN1         9       --   A2IN2
+  2        --  A1IN2         10      --   STBY2
+  3        --  STBY1         11      --   B2IN1
+  4        --  B1IN1         12      --   B2IN2
+  5        --  B1IN2         13      --   PWMB2
+  6        --  PWMB1         14      --   IO14
+  7        --  PWMA2         15      --   IO15
+
+ ****************************************************/
+
 #include <Wire.h>
-#include <Adafruit_GFX.h>
-#include <Adafruit_SSD1306.h>
 #include <Adafruit_PWMServoDriver.h>
-#include <SparkFun_RFD77402_Arduino_Library.h>
 
-// Constants
-#define MOTOR 0x40
-#define SERVO 0x41
-#define OLED_RESET 4
-#define SERVOMIN 150
-#define SERVOMAX 550
+// called this way, it uses the default address 0x40
+Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver();
+// you can also call it with a different address you want
+//Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver(0x41);
 
-// Objects and variables
-RFD77402 myDistance;
-Adafruit_SSD1306 display(OLED_RESET);
-Adafruit_PWMServoDriver pwm;
-uint8_t servonum = 0;
-uint16_t L_pulselen = 0;
-uint16_t R_pulselen = 0;
-unsigned int distance[5] = {0, 0, 0, 0, 0};
+// Depending on your servo make, the pulse width min and max may vary, you 
+// want these to be as small/large as possible without hitting the hard stop
+// for max range. You'll have to tweak them as necessary to match the servos you
+// have!
+#define SERVOMIN  1000 // this is the 'minimum' pulse length count (out of 4096)
+#define SERVOMAX  3000 // this is the 'maximum' pulse length count (out of 4096)
 
-// Function declarations
-void setMotorSpeed(uint8_t motorNum, uint16_t pulseLength);
-void moveServo(uint16_t angle);
-void measureDistance(uint8_t index, unsigned int *distanceArray);
-void goAhead();
-void stopMotors();
-void turnRight();
-void turnLeft();
+// our servo # counter
+//uint8_t servonum = 0;
+//uint8_t i = 0;
+uint16_t pulselen = 1000;
 
-void setup()
-{
+void setup() {
   Serial.begin(9600);
+  Serial.println("4 channel Motor test!");
 
-  // Initialize PWM driver for motor and servo
   pwm.begin();
-  pwm.setPWMFreq(60); // Analog servos run at ~60 Hz updates
+  pwm.setPWMFreq(60);  // Analog servos run at ~60 Hz updates
+
   yield();
-
-  // Initialize distance sensor
-  if (!myDistance.begin())
-  {
-    Serial.println("Sensor failed to initialize. Check wiring.");
-    while (1)
-      ; // Freeze!
-  }
-
-  Serial.println("Sensor online!");
 }
 
-void loop()
+// you can use this function if you'd like to set the pulse length in seconds
+// e.g. setServoPulse(0, 0.001) is a ~1 millisecond pulse width. its not precise!
+//void setServoPulse(uint8_t n, double pulse) {
+//  double pulselength;
+//  
+//  pulselength = 1000000;   // 1,000,000 us per second
+//  pulselength /= 60;   // 60 Hz
+//  Serial.print(pulselength); Serial.println(" us per period"); 
+//  pulselength /= 4096;  // 12 bits of resolution
+//  Serial.print(pulselength); Serial.println(" us per bit"); 
+//  pulse *= 1000;
+//  pulse /= pulselength;
+//  Serial.println(pulse);
+//  pwm.setPWM(n, 0, pulse);
+//}
+
+void  goahead()
 {
-  // Move the servo to the center position
-  moveServo(90);
-  delay(50);
+  pwm.setPin(3,0,1);    //H  STBY1  
+  pwm.setPin(1,0,1);    //H
+  pwm.setPin(2,4095,1); //L
+  pwm.setPin(4,0,1);    //H        
+  pwm.setPin(5,4095,1); //L
+    
+  pwm.setPin(10,0,1);    //H  STBY2  
+  pwm.setPin(8,0,1);    //H
+  pwm.setPin(9,4095,1); //L
+  pwm.setPin(11,0,1);    //H        
+  pwm.setPin(12,4095,1); //L
+  }
 
-  // Measure distance using the RFD77402 sensor
-  myDistance.takeMeasurement();
-  distance[2] = myDistance.getDistance();
-  Serial.println(distance[0]);
+void  goback()
+{
+  pwm.setPin(3,0,1);    //H  STBY1  
+  pwm.setPin(2,0,1);    //H
+  pwm.setPin(1,4095,1); //L
+  pwm.setPin(5,0,1);    //H        
+  pwm.setPin(4,4095,1); //L
+    
+  pwm.setPin(10,0,1);    //H  STBY2  
+  pwm.setPin(9,0,1);    //H
+  pwm.setPin(8,4095,1); //L
+  pwm.setPin(12,0,1);    //H        
+  pwm.setPin(11,4095,1); //L
+  }  
 
-  if (distance[2] > 300)
+ void stp()
+ {
+  pwm.setPin(3,0,0);     //L  STBY1
+  pwm.setPin(10,0,0);    //L  STBY2
+  }
+
+void loop() {
+  for(pulselen=1000;pulselen<4096;pulselen+=100)
   {
-    // Move forward
-    goAhead();
-    L_pulselen = R_pulselen = distance[2] + 200;
-    setMotorSpeed(7, L_pulselen);
-    setMotorSpeed(13, R_pulselen);
-
-    while (distance[2] > 300)
-    {
-      myDistance.takeMeasurement();
-      distance[2] = myDistance.getDistance();
-      // Move the servo to the center position
-      moveServo(90);
+    goahead();
+    pwm.setPWM(0, 0, pulselen);
+    pwm.setPWM(6, 0, pulselen);
+    pwm.setPWM(7, 0, pulselen);
+    pwm.setPWM(13, 0, pulselen);
+    delay(100);
     }
-  }
-  else if (distance[2] <= 300)
+  delay(500);
+  stp();
+  delay(2000);
+
+    for(pulselen=1000;pulselen<4096;pulselen+=100)
   {
-    // Stop motors
-    stopMotors();
-    // Move the servo to the extreme left position
-    moveServo(0);
-    delay(600);
-    // Measure distance to the left
-    measureDistance(0, distance);
-    // Move the servo to the extreme right position
-    moveServo(180);
-    delay(600);
-    // Measure distance to the right
-    measureDistance(4, distance);
-    // Move the servo to the center position
-    moveServo(90);
-    delay(600);
-
-    if ((distance[0] > 300 && distance[4] > 300 && distance[0] > distance[4]) ||
-        (distance[0] > 300 && distance[4] <= 300))
-    {
-      // Turn right
-      turnRight();
-      L_pulselen = R_pulselen = 600;
-      setMotorSpeed(7, L_pulselen);
-      setMotorSpeed(13, R_pulselen);
-
-      while (distance[2] <= 300)
-      {
-        while (distance[3] <= 300)
-        {
-          // Move the servo to the right position
-          moveServo(120);
-          delay(100);
-          // Measure distance to the right
-          measureDistance(3, distance);
-        }
-
-        // Move the servo to the center position
-        moveServo(90);
-        delay(100);
-        // Measure distance to the center
-        measureDistance(2, distance);
-      }
+    goback();
+    pwm.setPWM(0, 0, pulselen);
+    pwm.setPWM(6, 0, pulselen);
+    pwm.setPWM(7, 0, pulselen);
+    pwm.setPWM(13, 0, pulselen);
+    delay(100);
     }
-    else if ((distance[0] > 300 && distance[4] > 300 && distance[0] < distance[4]) ||
-             (distance[0] < 300 && distance[4] >= 300))
-    {
-      // Turn left
-      turnLeft();
-      L_pulselen = R_pulselen = 600;
-      setMotorSpeed(7, L_pulselen);
-      setMotorSpeed(13, R_pulselen);
-
-      while (distance[2] < 400)
-      {
-        while (distance[1] <= 300)
-        {
-          // Move the servo to the left position
-          moveServo(60);
-          delay(100);
-          // Measure distance to the left
-          measureDistance(1, distance);
-        }
-
-        // Move the servo to the center position
-        moveServo(90);
-        delay(100);
-        // Measure distance to the center
-        measureDistance(2, distance);
-      }
-    }
-  }
-  else
-  {
-    // Stop motors
-    stopMotors();
-  }
-}
-
-// Function to set the motor speed using the Adafruit_PWMServoDriver
-void setMotorSpeed(uint8_t motorNum, uint16_t pulseLength)
-{
-  pwm.setPWM(motorNum, 0, pulseLength);
-}
-
-// Function to move the servo to a specific angle
-void moveServo(uint16_t angle)
-{
-  uint16_t pulselen = map(angle, 0, 180, SERVOMIN, SERVOMAX);
-  pwm.setPWM(servonum, 0, pulselen);
-}
-
-// Function to measure distance and store it in the distance array
-void measureDistance(uint8_t index, unsigned int *distanceArray)
-{
-  myDistance.takeMeasurement();
-  distanceArray[index] = myDistance.getDistance();
-}
-
-// Functions for different movements
-void goAhead()
-{
-  // Implement forward movement logic
-  // ...
-}
-
-void stopMotors()
-{
-  // Implement logic to stop motors
-  // ...
-}
-
-void turnRight()
-{
-  // Implement right turn logic
-  // ...
-}
-
-void turnLeft()
-{
-  // Implement left turn logic
-  // ...
+  delay(500);
+  stp();
+  delay(2000);
 }
